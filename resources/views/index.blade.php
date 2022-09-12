aeb765758145731e0402a00dba5f9f8e54ff470c
MaulanaAlirridlo <maulana.alirridlo@gmail.com>
2022-09-12 15:27:58 +0700

    add laravel/ui

---
+++ resources/views/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.master')
+
+@section('title', 'Lowongan Kerja')
+
+@section('main')
+    <p>This is my body content.</p>
+@stop
--- webpack.mix.js
+++ webpack.mix.js
@@ -6,12 +6,11 @@ const mix = require('laravel-mix');
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
+ | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
  */
 
-mix.js('vendor/twbs/bootstrap/dist/js/bootstrap.min.js', 'public/js/app.js')
-    .postCss('vendor/twbs/bootstrap/dist/css/bootstrap.min.css', 'public/css/app.css', [
-        //
-    ]);
+mix.js('resources/js/app.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css')
+    .sourceMaps();

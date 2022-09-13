3a170e8637b675fca4bf02e3d20b246e0d7b00f8
MaulanaAlirridlo <maulana.alirridlo@gmail.com>
2022-09-13 14:04:13 +0700

    Add main

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,16 +4,90 @@
 
 @section('main')
     <div class="container">
-        <p>This is my body content.</p>
+        <h1 class="text-center text-main">Tempat Mencari Kerja</h1>
+        <p class="text-center text-main">Temukan loker Jogja terbaru bulan Juni 2021 dengan mudah.</p>
 
-        <div class="owl-carousel loop">
-            <div class="bg-success"> Your Content </div>
-            <div class="bg-success"> Your Content </div>
-            <div class="bg-success"> Your Content </div>
-            <div class="bg-success"> Your Content </div>
-            <div class="bg-success"> Your Content </div>
-            <div class="bg-success"> Your Content </div>
-            <div class="bg-success"> Your Content </div>
+        <div class="bg-white shadow rounded w-50 p-4 m-5 mx-auto">
+            <form action="">
+                <div class="row">
+                    <div class="col">
+                        <input type="text" class="form-control" placeholder="Cari Disini">
+                    </div>
+                    <div class="col">
+                        <select class="select" name="area" id="">
+                            <option value="0">All Areas</option>
+                        </select>
+                    </div>
+                    <div class="col">
+                        <select class="select" name="area" id="">
+                            <option value="0">All Categories</option>
+                        </select>
+                    </div>
+                </div>
+                <button class="btn bg-main rounded m-auto px-5 text-white d-flex mx-auto mt-4" type="submit" >
+                    <i class="fa-solid fa-magnifying-glass glass mx-1"></i> Search
+                </button>
+            </form>
+        </div>
+
+        <h2 class="text-center">Rekomendasi Lowongan</h2>
+        <div class="owl-carousel loop my-3">
+            <div class="rounded shadow-lg p-4 list-lowongan">
+                <h5 class="text-secondary">Dibutuhkan</h5>
+                <h5>Nama Pekerjaan</h5>
+                <img class="w-25 m-auto" src="{{asset('images/logo.png')}}" alt="">
+                <h5 class="border-bottom p-1">Nama Perusahaan</h5>
+                <h5 class="border-bottom p-1">Waktu</h5>
+                <h5 class="border-bottom p-1">Lokasi</h5>
+            </div>
+            <div class="rounded shadow-lg p-4 list-lowongan">
+                <h5 class="text-secondary">Dibutuhkan</h5>
+                <h5>Nama Pekerjaan</h5>
+                <img class="w-25 m-auto" src="{{asset('images/logo.png')}}" alt="">
+                <h5 class="border-bottom p-1">Nama Perusahaan</h5>
+                <h5 class="border-bottom p-1">Waktu</h5>
+                <h5 class="border-bottom p-1">Lokasi</h5>
+            </div>
+            <div class="rounded shadow-lg p-4 list-lowongan">
+                <h5 class="text-secondary">Dibutuhkan</h5>
+                <h5>Nama Pekerjaan</h5>
+                <img class="w-25 m-auto" src="{{asset('images/logo.png')}}" alt="">
+                <h5 class="border-bottom p-1">Nama Perusahaan</h5>
+                <h5 class="border-bottom p-1">Waktu</h5>
+                <h5 class="border-bottom p-1">Lokasi</h5>
+            </div>
+            <div class="rounded shadow-lg p-4 list-lowongan">
+                <h5 class="text-secondary">Dibutuhkan</h5>
+                <h5>Nama Pekerjaan</h5>
+                <img class="w-25 m-auto" src="{{asset('images/logo.png')}}" alt="">
+                <h5 class="border-bottom p-1">Nama Perusahaan</h5>
+                <h5 class="border-bottom p-1">Waktu</h5>
+                <h5 class="border-bottom p-1">Lokasi</h5>
+            </div>
+            <div class="rounded shadow-lg p-4 list-lowongan">
+                <h5 class="text-secondary">Dibutuhkan</h5>
+                <h5>Nama Pekerjaan</h5>
+                <img class="w-25 m-auto" src="{{asset('images/logo.png')}}" alt="">
+                <h5 class="border-bottom p-1">Nama Perusahaan</h5>
+                <h5 class="border-bottom p-1">Waktu</h5>
+                <h5 class="border-bottom p-1">Lokasi</h5>
+            </div>
+            <div class="rounded shadow-lg p-4 list-lowongan">
+                <h5 class="text-secondary">Dibutuhkan</h5>
+                <h5>Nama Pekerjaan</h5>
+                <img class="w-25 m-auto" src="{{asset('images/logo.png')}}" alt="">
+                <h5 class="border-bottom p-1">Nama Perusahaan</h5>
+                <h5 class="border-bottom p-1">Waktu</h5>
+                <h5 class="border-bottom p-1">Lokasi</h5>
+            </div>
+            <div class="rounded shadow-lg p-4 list-lowongan">
+                <h5 class="text-secondary">Dibutuhkan</h5>
+                <h5>Nama Pekerjaan</h5>
+                <img class="w-25 m-auto" src="{{asset('images/logo.png')}}" alt="">
+                <h5 class="border-bottom p-1">Nama Perusahaan</h5>
+                <h5 class="border-bottom p-1">Waktu</h5>
+                <h5 class="border-bottom p-1">Lokasi</h5>
+            </div>
         </div>
     </div>
 @stop
@@ -22,9 +96,9 @@
 <script>
     $('.loop').owlCarousel({
         center: true,
-        items:2,
+        items:4,
         loop:true,
-        margin:10,
+        margin:0,
         responsive:{
             600:{
                 items:4
